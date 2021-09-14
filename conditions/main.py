9c90c8d8e330da9c1135a4bdba2488caefa7135b
nvando <nikievandorst@gmail.com>
2021-09-14 22:27:16 +0200

    Exercise Conditions, second version

--- conditions/main.py
+++ conditions/main.py
@@ -7,24 +7,24 @@ __human_name__ = "conditions"
 # weather, time_of_day, milking_status, location, season, tank, grass
 
 
-def farm_action(weather, time_of_day, milking_status, location, season, tank, grass):
+def farm_action(weather, time, milking_status, location, season, full_tank, long_grass):
     if location == "pasture":
-        if weather == "rainy" or time_of_day == "night":
+        if weather == "rainy" or time == "night":
             return "take cows to cowshed"
         elif milking_status:
             return "take cows to cowshed\nmilk cows\ntake cows back to pasture"
-        elif tank == True and (weather != "sunny" and weather != "windy"):
+        elif full_tank and (weather != "sunny" and weather != "windy"):
             return "take cows to cowshed\nfertilize pasture\ntake cows back to pasture"
-        elif grass == True and season == "spring" and weather == "sunny":
+        elif long_grass and season == "spring" and weather == "sunny":
             return "take cows to cowshed/nmow grass\ntake cows back to pasture"
         else:
             return "wait"
     if location == "cowshed":
         if milking_status:
             return "milk cows"
-        elif tank == True and (weather != "sunny" and weather != "windy"):
+        elif full_tank and (weather != "sunny" and weather != "windy"):
             return "fertilize pasture"
-        elif grass == True and season == "spring" and weather == "sunny":
+        elif long_grass and season == "spring" and weather == "sunny":
             return "mow grass"
         else:
             return "wait"
