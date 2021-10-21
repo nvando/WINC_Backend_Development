982eaddd95d752ba8f5aaadd1b3351d3fed56b60
nvando <nikievandorst@gmail.com>
2021-10-21 10:31:05 +0200

    2nd version. Added ValueError handling in class Player and shortened instance method 'strenght'. Also added instance method compare_players to class Commentator.

--- classes/main.py
+++ classes/main.py
@@ -6,13 +6,12 @@ __human_name__ = "classes"
 class Player:
     def __init__(self, name, speed, endurance, accuracy):
         self.name = name
-        # self.speed = speed
-        self.endurance = endurance
-        self.accuracy = accuracy
-        # if (speed or endurance or accuracy) > 1:
-        #     raise ValueError
         if 0 < speed < 1:
             self.speed = speed
+        if 0 < endurance < 1:
+            self.endurance = endurance
+        if 0 < accuracy < 1:
+            self.accuracy = accuracy
         else:
             raise ValueError
 
@@ -20,16 +19,16 @@ class Player:
         return f"Hello everyone, my name is {self.name}."
 
     def strength(self):
+        # returns a tuple with the player's highest attribute
+        # and the corresponding attribute value.
+        # If multiple attributes share the same value,
+        # prioritize by speed > endurance > accuracy.
         if self.speed >= self.endurance and self.speed >= self.accuracy:
-            highest_name = "speed"
-            highest_value = self.speed
+            return ("speed", self.speed)
         elif self.endurance > self.speed and self.endurance >= self.accuracy:
-            highest_name = "endurance"
-            highest_value = self.endurance
-        elif self.accuracy > self.speed and self.accuracy > self.endurance:
-            highest_name = "accuracy"
-            highest_value = self.accuracy
-        return (highest_name, highest_value)
+            return ("endurance", self.endurance)
+        else:
+            return ("accuracy", self.accuracy)
 
 
 class Commentator:
@@ -40,13 +39,34 @@ class Commentator:
         return player.speed + player.endurance + player.accuracy
 
     def compare_players(self, player1, player2, attribute):
-        pass
+        # returns the name of the player that scores the highest
+        # on the attribute argument. If the players score equally,
+        # return the player that has the highest strength.
+        # If these are also equal, report the player
+        # who has the highest total score according to sum_player.
+        if getattr(player1, attribute) > getattr(player2, attribute):
+            return player1.name
+        if getattr(player1, attribute) < getattr(player2, attribute):
+            return player2.name
+        else:
+            if player1.strength()[1] > player2.strength()[1]:
+                return player1.name
+            if player1.strength()[1] < player2.strength()[1]:
+                return player2.name
+            else:
+                if self.sum_player(player1) < self.sum_player(player2):
+                    return player1.name
+                if self.sum_player(player1) < self.sum_player(player2):
+                    return player2.name
+                else:
+                    return "These two players might as well be twins!"
 
 
 if __name__ == "__main__":
-    player1 = Player("Bob", 0.2, 0.5, 0.8)
-    Player2 = Player("Rita", 0.1, 0.9, 0.7)
-    print(Player2.introduce())
-    print(Player2.strength())
+    player1 = Player("Bob", 0.1, 0.5, 0.8)
+    player2 = Player("Rita", 0.1, 0.5, 0.8)
+    print(player2.introduce())
+    print(player2.strength())
     ray = Commentator("Ray Hudson")
-    print(ray.sum_player(Player2))
+    print(ray.sum_player(player2))
+    print(ray.compare_players(player1, player2, "accuracy"))
