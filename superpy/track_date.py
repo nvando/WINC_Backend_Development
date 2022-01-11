69d033313b3a4af3c34a2466786db793646074b2
nvando <nikievandorst@gmail.com>
2022-01-11 15:21:11 +1100

    Updated track_date.py with 'valid-month' and 'valid-year' functions which are used in parser.py to validate whether a CLI command includes a date (specific month or year) in the right format

--- superpy/track_date.py
+++ superpy/track_date.py
@@ -1,24 +1,34 @@
 from pathlib import Path
 from datetime import datetime, date, timedelta
 import csv
+import sys
 
-# create a 'today file' if it does not exist
-# or open it to retrieve the date that systems has stored as 'today'
-if not Path("today.csv").is_file():
-    TODAY = date.today()  # date-object does not include the time part
+
+# ------------------------------------------------------------------------------------
+# Below functionality retrieves the date from today.csv
+# in other to keep track of TODAY
+
+
+if not Path("today.csv").is_file():  # create a 'today file' if it does not exist
+    TODAY = date.today()
     with open("today.csv", "w") as file:
         writer = csv.writer(file)
         writer.writerow(["todays_date", TODAY])
-else:
+
+else:  # open today.csv to retrieve the date that has been stored as 'today'
     with open("today.csv", "r") as file:
         reader = csv.reader(file)
         TODAY = datetime.strptime(next(reader)[1], "%Y-%m-%d").date()
 
 
+# -------------------------------------------------------------------------------------
+# Below functions validate whether the CL arguments are in correct date format
+
+
 def valid_date(date_str):
     """validates whether the input date has format 'YYYY-MM-DD'
     or converts 'today' or 'yesterday' strings to a valid date.
-    returns date-time object"""
+    returns date object"""
 
     if date_str == "today":
         date_obj = TODAY
@@ -36,4 +46,34 @@ def valid_date(date_str):
                 f"{date_str} not a valid_date.",
                 "Please enter a date in format 'YYYY-MM-DD'",
             )
-            quit()
+            sys.exit()
+
+
+def valid_month(date_str):
+    """validates whether the input date has format 'YYYY-MM', returns date object"""
+
+    try:
+        return datetime.strptime(date_str, "%Y-%m").date()
+    except ValueError:
+        print(
+            f"{date_str} not a valid month.",
+            "Please enter a date in format 'YYYY-MM'",
+        )
+        sys.exit()
+
+
+def valid_year(date_str):
+    """validates whether the input date has format 'YYYY-MM', returns date object"""
+
+    try:
+        return datetime.strptime(date_str, "%Y").date()
+    except ValueError:
+        print(
+            f"{date_str} not a valid year.",
+            "Please enter a date in format 'YYYY'",
+        )
+        sys.exit()
+
+
+# utility functions: seperated from functions in main.py which
+# are called directly by the command line arguments
